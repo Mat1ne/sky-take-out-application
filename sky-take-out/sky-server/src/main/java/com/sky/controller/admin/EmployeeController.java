70d4acb9ff933b1775f1ab4c5950829f89fb97ef
zaoanmatong <2685539245@qq.com>
2026-06-10 13:52:08 +0800

    启用禁用员工业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -11,6 +11,7 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,7 +86,6 @@ public class EmployeeController {
 
     /**
      * 员工分页查询
-     * @param employeeDTO
      * @return
      */
     @GetMapping("/page")
@@ -94,5 +94,19 @@ public class EmployeeController {
         log.info("员工分页查询，参数为: {}" , employeePageQueryDTO);
         PageResult pageResult =  employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status , Long id){
+        log.info("启用禁用员工账号");
+        employeeService.startOrStop(status , id);
+        return Result.success();
     }
 }
