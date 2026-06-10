0799161d8c2737c36bdd2d1b9018c43dcde91116
zaoanmatong <2685539245@qq.com>
2026-06-10 22:01:42 +0800

    编辑员工业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
-
+//cd /Users/zaoanmatong/Downloads/nginx-macos-sky
+//./start.sh
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
@@ -109,4 +110,20 @@ public class EmployeeController {
         employeeService.startOrStop(status , id);
         return Result.success();
     }
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息");
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+    @PutMapping
+    @ApiOperation("编辑员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息: {}" , employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 }
